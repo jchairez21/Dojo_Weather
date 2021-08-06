9e7e82b39bf7a529284a30ceb0985db878f1f880
Joseph Chairez <joseph_chairez@yahoo.com>
2021-08-06 16:22:37 -0700

    initiat commit

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 console.log("page loading...");
+
+function loading() {
+  alert("Loading weather report...");
+}
+
+var cookieSpan = document.querySelector(".btn");
+
+function accept() {
+  cookieSpan.remove();
+}
+
+var endedIt = document.querySelector(".sub_container");
+
+function hide() {
+  endedIt.remove();
+}
+
+function convert(element) {
+  console.log(element.value);
+}
+
+function over(element) {
+  element.style.backgroundColor = "lime";
+}
+
+function out(element) {
+  element.style.backgroundColor = "#efefef";
+}
